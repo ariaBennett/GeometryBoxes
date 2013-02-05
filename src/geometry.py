4e64301d47cd96a6a275c514969d3be3f2094834
Aria Bennett <twoplustwo@gmail.com>
2013-02-05 14:49:19 -0800

    first commit

---
+++ src/geometry.py
@@ -0,0 +1,12 @@
+'''
+Geometry.py
+Created on Feb 5, 2013
+@author: Aria Bennett
+
+This module deals with creating rectangles 
+(Which are sometimes also squares!) and doing
+questionable things to them.
+'''
+
+if __name__ == '__main__':
+    print "tes"
